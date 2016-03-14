7a58367a5a0edb2968fbe59e244c02a9ca02d614
David Mancini <mancini.david@gmail.com>
2016-03-14 11:56:00 -0600

    Attempting to debug row-eq-height

--- test.php
+++ test.php
@@ -14,146 +14,49 @@
         <p><a class="btn btn-primary btn-lg" href="#" role="button"><i class="fa fa-cutlery"></i>&nbsp;Our Menu</a></p>
       </div>
 
+      <div class="row">
+				<div class="col-sm-6 col-xs-12">
+					<div class="boxHeading">
+						<span class="boxHeading"><i class="fa fa-clock-o"></i>&nbsp;Hours</span>
+					</div>
+					<div class="boxContent">
+            <table class="table text-center">
+              <tr><td>Mon</td><td>7:30 am - 5:30 pm</td></tr>
+              <tr><td>Tue</td><td>7:30 am - 5:30 pm</td></tr>
+              <tr><td>Wed</td><td>7:30 am - 5:30 pm</td></tr>
+              <tr><td>Thu</td><td>7:30 am - 5:30 pm</td></tr>
+              <tr><td>Fri</td><td>7:30 am - 5:30 pm</td></tr>
+              <tr><td>Sat</td><td>8:00 am - 3:30 pm</td></tr>
+              <tr><td>Sun</td><td>Closed</td></tr>
+            </table>
+					</div>
+				</div>
 
-<div class="row row-eq-height">
-  <div class="col-md-6">
-    <div class="panelBoxHeading">
-      <span class="panelHeading"><i class="fa fa-clock-o"></i>&nbsp;Hours</span>
-    </div>
-    <div class="panelContent">
-      <table class="table text-center">
-        <tr><td>Mon</td><td>7:30 am - 5:30 pm</td></tr>
-        <tr><td>Tue</td><td>7:30 am - 5:30 pm</td></tr>
-        <tr><td>Wed</td><td>7:30 am - 5:30 pm</td></tr>
-        <tr><td>Thu</td><td>7:30 am - 5:30 pm</td></tr>
-        <tr><td>Fri</td><td>7:30 am - 5:30 pm</td></tr>
-        <tr><td>Sat</td><td>8:00 am - 3:30 pm</td></tr>
-        <tr><td>Sun</td><td>Closed</td></tr>
-      </table>
-      <div class="text-center"><a class="btn btn-primary" id="priceBoxButtonPopular" href="#quote" role="button">Get Started!</a></div><br>
-    </div>
-  </div>
-  <div class="col-md-6">
-    <div class="panelHeading">
-      <span class="panelHeading"><i class="fa fa-clock-o"></i>&nbsp;Hours</span>
-    </div>
-    <div class="panelContent">
-      <table class="table table-striped">
-        <tr><td><i class="fa fa-check"></i> Responsive Design</td></tr>
-        <tr><td><i class="fa fa-check"></i> Mobile Friendly</td></tr>
-        <tr><td><i class="fa fa-check"></i> Custom Domain Name</td></tr>
-        <tr><td><i class="fa fa-check"></i> 1 Year Free Hosting</td></tr>
-        <tr><td><i class="fa fa-check"></i> 5 Domain Email Addresses</td></tr>
-        <tr><td><i class="fa fa-check"></i> Up to 10 Pages</td></tr>
-        <tr><td><i class="fa fa-check"></i> Premium Wordpress Theme</td></tr>
-        <tr><td><i class="fa fa-times"></i> Search Engine Optimization Marketing</td></tr>
-      </table>
-      <div class="text-center"><a class="btn btn-primary" id="priceBoxButtonPopular" href="#quote" role="button">Get Started!</a></div><br>
-    </div>
-  </div>
-</div>
+        <div class="col-sm-6 col-xs-12">
+					<div class="boxHeading">
+						<span class="boxHeading"><i class="fa fa-camera"></i>&nbsp;Photos</span>
+					</div>
 
-
-
-
-
-
-      <div class="row row-eq-height">
-        <div class="col-md-6">
-          <div class="panel panel-default">
-            <div class="panel-heading colorHeading">
-              <h3 class="panel-title"><i class="fa fa-clock-o"></i>&nbsp;Hours</h3>
-            </div>
-            <div class="panel-body">
-              <table class="table text-center">
-                <tr><td>Mon</td><td>7:30 am - 5:30 pm</td></tr>
-                <tr><td>Tue</td><td>7:30 am - 5:30 pm</td></tr>
-                <tr><td>Wed</td><td>7:30 am - 5:30 pm</td></tr>
-                <tr><td>Thu</td><td>7:30 am - 5:30 pm</td></tr>
-                <tr><td>Fri</td><td>7:30 am - 5:30 pm</td></tr>
-                <tr><td>Sat</td><td>8:00 am - 3:30 pm</td></tr>
-                <tr><td>Sun</td><td>Closed</td></tr>
-              </table>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-6">
-          <div class="panel panel-default">
-            <div class="panel-heading colorHeading">
-              <h3 class="panel-title"><i class="fa fa-camera"></i>&nbsp;Photos</h3>
-            </div>
-            <div class="panel-body text-center">
-              <div class="row">
+					<div class="boxContent text-center">
+            <div class="text-center">
                 <a href="images/burger.jpg" class="swipebox" title="Green Chili Burger">
-                  <img src="images/thumbnails/burger.jpg" alt="image" class="swipeboxImage">
-                </a>
+                  <img src="images/thumbnails/burger.jpg" alt="image" class="swipeboxImage"></a>
                 <a href="images/chicken.jpg" class="swipebox" title="Chicken Skewer">
-                  <img src="images/thumbnails/chicken.jpg" alt="image" class="swipeboxImage">
-                </a>
+                  <img src="images/thumbnails/chicken.jpg" alt="image" class="swipeboxImage"></a>
                 <a href="images/greek-salad.jpg" class="swipebox" title="Greek Salad">
-                  <img src="images/thumbnails/greek-salad.jpg" alt="image" class="swipeboxImage">
-                </a>
-              </div>
+                  <img src="images/thumbnails/greek-salad.jpg" alt="image" class="swipeboxImage"></a>
 
-              <div class="row">
                 <a href="images/gyros.jpg" class="swipebox" title="Gyros">
-                  <img src="images/thumbnails/gyros.jpg" alt="image" class="swipeboxImage">
-                </a>
+                  <img src="images/thumbnails/gyros.jpg" alt="image" class="swipeboxImage"></a>
                 <a href="images/lamb.jpg" class="swipebox" title="Lamb">
-                  <img src="images/thumbnails/lamb.jpg" alt="image" class="swipeboxImage">
-                </a>
+                  <img src="images/thumbnails/lamb.jpg" alt="image" class="swipeboxImage"></a>
                 <a href="images/shrimp.jpg" class="swipebox" title="Shrimp Plate">
-                  <img src="images/thumbnails/shrimp.jpg" alt="image" class="swipeboxImage">
-                </a>
-              </div>
+                  <img src="images/thumbnails/shrimp.jpg" alt="image" class="swipeboxImage"></a>
             </div>
           </div>
-        </div>
-      </div><!--/row-->
 
-
-      <div class="row">
-        <div class="col-md-6">
-          <div class="panel panel-default">
-            <div class="panel-heading colorHeading">
-              <h3 class="panel-title"><i class="fa fa-map-marker"></i>&nbsp;Location</h3>
-            </div>
-            <div class="panel-body">
-              <p><a href="tel:5052472323"><i class="fa fa-phone"></i>&nbsp;Call Us: (505) 247-2323</a></p>
-
-              <p>20 First Plaza NW, Suite 2246<br>Albuquerque, NM 87102</p>
-              <iframe class="center-block" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6529.653793977849!2d-106.65297739483779!3d35.086057649987914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8722655878967191%3A0xd56f937712199b86!2sZohra+Restaurant!5e0!3m2!1sen!2sus!4v1457747863065" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-6">
-          <div class="panel panel-default">
-            <div class="panel-heading colorHeading">
-              <h3 class="panel-title"><i class="fa fa-yelp"></i>&nbsp;Reviews</h3>
-            </div>
-            <div class="panel-body">
-              <div id='yelpwidget'></div>
-            </div>
-          </div>
-        </div>
-
-
-      </div><!--/row-->
-
-      <div class="row">
-        <div class="col-md-12">
-          <div class="panel panel-default">
-            <div class="panel-heading colorHeading">
-              <h3 class="panel-title"><i class="fa fa-heart"></i>&nbsp;About Zohra</h3>
-            </div>
-            <div class="panel-body">
-
-            </div>
-          </div>
-        </div>
-      </div>
+				</div><!--/photos-->
+			</div><!--/row-->
 
 
 

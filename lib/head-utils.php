fe1270d97aeab94d5daa370bd27eca3bc5153072
David Mancini <mancini.david@gmail.com>
2016-03-14 09:33:23 -0600

    Pushed files previously on BitBucket

---
+++ lib/head-utils.php
@@ -0,0 +1,34 @@
+<!--jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+
+
+<!-- Tells IE to use highest mode available, avoiding IE compatibility Mode bugs. -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<script>window.onkeypress = function(event) {if (event.keyCode === 169) {alert('This site was developed by David Mancini.');}};</script>
+
+<!-- Sets viewport and zoom scale to 1:1.  This is required when creating a responsive site. -->
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<!--Font Awesome-->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<!--Yelp Widget-->
+<script>(function() { var s = document.createElement("script");s.async = true;s.onload = s.onreadystatechange = function(){getYelpWidget("zohra-authentic-foods-albuquerque","300","RED","y","y","2");};s.src='http://chrisawren.com/widgets/yelp/yelpv2.js' ;var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);})();</script>
+
+<link rel="stylesheet" href="css/styles.css">
+<!-- <link rel="stylesheet" href="css/flexbox.css"> -->
+
+<!--Swipebox-->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script><script src="js/jquery.swipebox.min.js"></script>
+<script type="text/javascript">;( function( $ ) {$( '.swipebox' ).swipebox();} )( jQuery );</script>
+<link rel="stylesheet" href="css/swipebox.min.css">

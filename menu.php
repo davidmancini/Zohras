45fb878e3ce5ce2f83a94d87f2a07eb9e1b21b37
David Mancini <mancini.david@gmail.com>
2016-03-17 10:05:44 -0600

    Added breakfast menu.

--- menu.php
+++ menu.php
@@ -13,148 +13,208 @@
         <p><a class="btn btn-primary btn-lg" href="tel:5052472323" role="button"><i class="fa fa-phone"></i>&nbsp;Order Now: (505) 247-2323</a></p>
       </div>
       <p><a href="index.php"><i class="fa fa-arrow-circle-left"></i>&nbsp;Home</a></p>
+      <div class="breakfast">
+        <h2 class="text-center">Zohra's Breakfast Menu</h2>
+        <div class="row">
+          <div class="col-sm-6 col-xs-12">
+            <div class="boxHeading">
+              <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Breakfast</span>
+            </div>
+            <div class="boxContent center-block">
+              <table class="table-responsive table-striped table-hover text-center" width="100%">
+                <tr><td>Cheese Omlet</td><td>$7.00</td></tr>
+                <tr><td>Afghan Style Omlet<br>Egg, light spicy sausage, cilantro, and cucumber</td><td>$7.00</td></tr>
+                <tr><td>Breakfast Burrito<br>Egg, cheese, green chili, and has brown</td><td>$4.00</td></tr>
+                <tr><td>Bagel and Cream Cheese</td><td>$2.50</td></tr>
+                <tr><td>Safwon's Breakfast<br>Scrambled eggs and cheese on a toasted bagel</td><td>$5.00</td></tr>
+              </table>
+            </div>
+          </div>
+          <div class="col-sm-6 col-xs-12">
+            <div class="boxHeading">
+              <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Sides</span>
+            </div>
+            <div class="boxContent center-block">
+              <table class="table-responsive table-striped table-hover text-center" width="100%">
+                <tr><td>Sausages (2 pieces)</td><td>$1.50</td></tr>
+                <tr><td>Bacon (2 pieces)</td><td>$1.50</td></tr>
+                <tr><td>Toast (2 pieces)</td><td>$1.50</td></tr>
+              </table>
+            </div>
+          </div>
+        </div><!--/row-->
+      </div><!--/breakfast-->
+
+      <div class="lunch">
+        <h2 class="text-center">Zohra's Lunch Menu</h2>
+        <div class="row">
+          <div class="col-sm-6 col-xs-12">
+            <div class="boxHeading">
+              <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Daily Specials</span>
+            </div>
+            <div class="boxContent">
+              <p>All our hamburgers are 100% sirloin, other sandwitches are made with natural meats and organic veggies
+              Meal includes sandwich, a bag of chips & a drink.</p>
+              <table class="table-responsive table-striped table-hover text-center" width="100%">
+                <tr><td>Gyro</td><td>$8.99</td></tr>
+                <tr><td>Chicken Gyro</td><td>$8.99</td></tr>
+                <tr><td>Hamburger</td><td>$8.35</td></tr>
+                <tr><td>Cheeseburger</td><td>$8.35</td></tr>
+                <tr><td>Green Chili Cheeseburger</td><td>$8.35</td></tr>
+              </table>
+            </div>
+          </div>
+
+          <div class="col-sm-6 col-xs-12">
+            <div class="boxHeading">
+              <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Delicious Authentic Food</span>
+            </div>
+            <div class="boxContent center-block">
+              <table class="table-responsive table-striped table-hover text-center" width="100%">
+                <tr><td>Gyro Sandwich</td><td>$5.99</td></tr>
+                <tr><td>Chicken Gyro Sandwich</td><td>$5.99</td></tr>
+                <tr><td>Hamburger</td><td>$5.50</td></tr>
+                <tr><td>Cheeseburger</td><td>$5.50</td></tr>
+                <tr><td>Green Chili Cheeseburger</td><td>$5.50</td></tr>
+                <tr><td>Grilled Chicken Sandwich</td><td>$6.00</td></tr>
+                <tr><td>BLT</td><td>$5.00</td></tr>
+                <tr><td>Chili Dog</td><td>$3.75</td></tr>
+                <tr><td>Hot Dog</td><td>$2.75</td></tr>
+                <tr><td>Chili Bowl (bean and beaf) with a Pita</td><td>$5.00</td></tr>
+                <tr><td>Indian Taco</td><td>$8.00</td></tr>
+                <tr><td>Frito Pie</td><td>$3.00</td></tr>
+              </table>
+            </div>
+          </div>
+       </div><!--/row-->
+
+       <div class="row">
+         <div class="col-sm-6 col-xs-12">
+          <div class="boxHeading">
+            <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Ethnic Cuisines</span>
+          </div>
+          <div class="boxContent">
+             <table class="table-responsive table-striped table-hover text-center" width="100%">
+               <tr><td><strong>Chicken Kabob (one stick)</strong></td><td><strong>$4.00</strong></td></tr>
+               <tr><td>Gyro over Rice</td><td>$9.00</td></tr>
+               <tr><td><strong>Chicken Kabob<br>over rice or pita.</strong></td><td><strong>$9.00</strong></td></tr>
+               <tr><td><strong>Signature</strong> Chicken Salan<br>cooked with curry, cardamom, fresh tomato,<br> mint, and onion over rice.</td><td>$5.00</td></tr>
+             </table>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-xs-12">
+          <div class="boxHeading">
+            <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Vegetarian Specials</span>
+          </div>
+          <div class="boxContent center-block">
+            <p>Made with organic vegetables and cooked in vegetable oil</p>
+             <table class="table-responsive table-striped table-hover text-center" width="100%">
+               <tr><td>Falafel Meal<br>(sandwich, back of chips & a drink)</td><td>$8.99</td></tr>
+               <tr><td>Falafel Sandwich</td><td>$6.00</td></tr>
+               <tr><td>Veggie Indian Taco</td><td>$8.00</td></tr>
+               <tr><td>Veggie Frito Pie</td><td>$3.00</td></tr>
+               <tr><td>Grilled Cheese Sandwich</td><td>$3.50</td></tr>
+               <tr><td>Boca Burger</td><td>$5.50</td></tr>
+               <tr><td>Veggie Beans and Rice</td><td>$5.00</td></tr>
+               <tr><td>Sabizi (Spinich w/ Rice)<br>Cooked with fresh cilantro, tofu, and spice</td><td>$7.00</td></tr>
+             </table>
+          </div>
+        </div>
+      </div><!--/row-->
 
       <div class="row">
-				<div class="col-sm-6 col-xs-12">
-					<div class="boxHeading">
-						<span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Daily Specials</span>
-					</div>
-					<div class="boxContent">
-            <p>All our hamburgers are 100% sirloin, other sandwitches are made with natural meats and organic veggies
-            Meal includes sandwich, a bag of chips & a drink.</p>
+       <div class="col-sm-6 col-xs-12">
+         <div class="boxHeading">
+           <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Gourmet Salads</span>
+         </div>
+         <div class="boxContent">
+           <p>Made with organic vegetables & natural meats</p>
             <table class="table-responsive table-striped table-hover text-center" width="100%">
-              <tr><td>Gyro</td><td>$8.99</td></tr>
-              <tr><td>Chicken Gyro</td><td>$8.99</td></tr>
-              <tr><td>Hamburger</td><td>$8.35</td></tr>
-              <tr><td>Cheeseburger</td><td>$8.35</td></tr>
-              <tr><td>Green Chili Cheeseburger</td><td>$8.35</td></tr>
+              <tr><td>Large Greek Salad</td><td>$6.00</td></tr>
+              <tr><td>Greek Grilled Chicken Salad</td><td>$9.00</td></tr>
+              <tr><td>Greek Gyro Salad</td><td>$9.00</td></tr>
+              <tr><td>Greek Falafel Salad</td><td>$9.00</td></tr>
             </table>
-					</div>
-				</div>
+         </div>
+       </div>
 
-  			<div class="col-sm-6 col-xs-12">
-  				<div class="boxHeading">
-  					<span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Delicious Authentic Food</span>
-  				</div>
-  				<div class="boxContent center-block">
+       <div class="col-sm-6 col-xs-12">
+         <div class="boxHeading">
+           <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Side Dishes</span>
+         </div>
+         <div class="boxContent center-block">
             <table class="table-responsive table-striped table-hover text-center" width="100%">
-              <tr><td>Gyro Sandwich</td><td>$5.99</td></tr>
-              <tr><td>Chicken Gyro Sandwich</td><td>$5.99</td></tr>
-              <tr><td>Hamburger</td><td>$5.50</td></tr>
-              <tr><td>Cheeseburger</td><td>$5.50</td></tr>
-              <tr><td>Green Chili Cheeseburger</td><td>$5.50</td></tr>
-              <tr><td>Grilled Chicken Sandwich</td><td>$6.00</td></tr>
-              <tr><td>BLT</td><td>$5.00</td></tr>
-              <tr><td>Chili Dog</td><td>$3.75</td></tr>
-              <tr><td>Hot Dog</td><td>$2.75</td></tr>
-              <tr><td>Chili Bowl (bean and beaf) with a Pita</td><td>$5.00</td></tr>
-              <tr><td>Indian Taco</td><td>$8.00</td></tr>
-              <tr><td>Frito Pie</td><td>$3.00</td></tr>
+              <tr><td>French Fries</td><td>$2.45</td></tr>
+              <tr><td>Afghan Rice</td><td>$3.50</td></tr>
+              <tr><td>Steamed Vegetables</td><td>$4.00</td></tr>
+              <tr><td>Flat Bread</td><td>$2.95</td></tr>
             </table>
-  				</div>
-  			</div>
+         </div>
+       </div>
      </div><!--/row-->
 
      <div class="row">
-       <div class="col-sm-6 col-xs-12">
+      <div class="col-sm-6 col-xs-12">
         <div class="boxHeading">
-          <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Ethnic Cuisines</span>
+          <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Add Extra</span>
         </div>
         <div class="boxContent">
            <table class="table-responsive table-striped table-hover text-center" width="100%">
-             <tr><td><strong>Chicken Kabob (one stick)</strong></td><td><strong>$4.00</strong></td></tr>
-             <tr><td>Gyro over Rice</td><td>$9.00</td></tr>
-             <tr><td><strong>Chicken Kabob<br>over rice or pita.</strong></td><td><strong>$9.00</strong></td></tr>
-             <tr><td><strong>Signature</strong> Chicken Salan<br>cooked with curry, cardamom, fresh tomato,<br> mint, and onion over rice.</td><td>$5.00</td></tr>
+             <tr><td>Extra Gyro Meat</td><td>$2.50</td></tr>
+             <tr><td>Extra Pita</td><td>$1.00</td></tr>
+             <tr><td>Extra Cheese</td><td>$0.35</td></tr>
+             <tr><td>Extra Gyro Sauce</td><td>$0.35</td></tr>
+             <tr><td>Extra Chili (Green or Red)</td><td>$0.35</td></tr>
            </table>
         </div>
       </div>
 
       <div class="col-sm-6 col-xs-12">
         <div class="boxHeading">
-          <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Vegetarian Specials</span>
+          <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Kids' Meals (Only ages 12 & under)</span>
         </div>
         <div class="boxContent center-block">
-          <p>Made with organic vegetables and cooked in vegetable oil</p>
            <table class="table-responsive table-striped table-hover text-center" width="100%">
-             <tr><td>Falafel Meal<br>(sandwich, back of chips & a drink)</td><td>$8.99</td></tr>
-             <tr><td>Falafel Sandwich</td><td>$6.00</td></tr>
-             <tr><td>Veggie Indian Taco</td><td>$8.00</td></tr>
-             <tr><td>Veggie Frito Pie</td><td>$3.00</td></tr>
-             <tr><td>Grilled Cheese Sandwich</td><td>$3.50</td></tr>
-             <tr><td>Boca Burger</td><td>$5.50</td></tr>
-             <tr><td>Veggie Beans and Rice</td><td>$5.00</td></tr>
-             <tr><td>Sabizi (Spinich w/ Rice)<br>Cooked with fresh cilantro, tofu, and spice</td><td>$7.00</td></tr>
+             <tr><td>Cheese Quesadilla, chips, soda</td><td>$5.00</td></tr>
+             <tr><td>Hot dog, chips, soda</td><td>$5.00</td></tr>
            </table>
         </div>
       </div>
     </div><!--/row-->
-
+  </div><!--/lunch-->
+  <h2>Beverages</h2>
+  <div class="beverages">
     <div class="row">
-     <div class="col-sm-6 col-xs-12">
-       <div class="boxHeading">
-         <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Gourmet Salads</span>
-       </div>
-       <div class="boxContent">
-         <p>Made with organic vegetables & natural meats</p>
+      <div class="col-sm-6 col-xs-12">
+        <div class="boxHeading">
+          <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Beverages</span>
+        </div>
+        <div class="boxContent center-block">
           <table class="table-responsive table-striped table-hover text-center" width="100%">
-            <tr><td>Large Greek Salad</td><td>$6.00</td></tr>
-            <tr><td>Greek Grilled Chicken Salad</td><td>$9.00</td></tr>
-            <tr><td>Greek Gyro Salad</td><td>$9.00</td></tr>
-            <tr><td>Greek Falafel Salad</td><td>$9.00</td></tr>
+            <tr><td>Bottle of Water</td><td>$1.50</td></tr>
+            <tr><td>Iced Tea (16 oz)</td><td>$2.00</td></tr>
+            <tr><td>Orange Juice (16 oz)</td><td>$2.50</td></tr>
+            <tr><td>Apple Juice (16 oz)</td><td>$2.50</td></tr>
+            <tr><td>Cranberry Juice (16 oz)</td><td>$2.50</td></tr>
+            <tr><td>Lemonade (16 oz)</td><td>$2.50</td></tr>
+            <tr><td>Milk (16 oz)</td><td>$2.50</td></tr>
+            <tr><td>Lipton Tea (16 oz)</td><td>$2.00</td></tr>
+            <tr><td>Coffee</td><td>$2.50</td></tr>
+            <tr><td>Can of Soda</td><td>$1.00</td></tr>
           </table>
-       </div>
-     </div>
-
-     <div class="col-sm-6 col-xs-12">
-       <div class="boxHeading">
-         <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Side Dishes</span>
-       </div>
-       <div class="boxContent center-block">
-          <table class="table-responsive table-striped table-hover text-center" width="100%">
-            <tr><td>French Fries</td><td>$2.45</td></tr>
-            <tr><td>Afghan Rice</td><td>$3.50</td></tr>
-            <tr><td>Steamed Vegetables</td><td>$4.00</td></tr>
-            <tr><td>Flat Bread</td><td>$2.95</td></tr>
-          </table>
-       </div>
-     </div>
-   </div><!--/row-->
-
-   <div class="row">
-    <div class="col-sm-6 col-xs-12">
-      <div class="boxHeading">
-        <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Add Extra</span>
+        </div>
       </div>
-      <div class="boxContent">
-         <table class="table-responsive table-striped table-hover text-center" width="100%">
-           <tr><td>Extra Gyro Meat</td><td>$2.50</td></tr>
-           <tr><td>Extra Pita</td><td>$1.00</td></tr>
-           <tr><td>Extra Cheese</td><td>$0.35</td></tr>
-           <tr><td>Extra Gyro Sauce</td><td>$0.35</td></tr>
-           <tr><td>Extra Chili (Green or Red)</td><td>$0.35</td></tr>
-         </table>
-      </div>
-    </div>
-
-    <div class="col-sm-6 col-xs-12">
-      <div class="boxHeading">
-        <span class="boxHeading"><i class="fa fa-cutlery"></i>&nbsp;Kids' Meals (Only ages 12 & under)</span>
-      </div>
-      <div class="boxContent center-block">
-         <table class="table-responsive table-striped table-hover text-center" width="100%">
-           <tr><td>Cheese Quesadilla, chips, soda</td><td>$5.00</td></tr>
-           <tr><td>Hot dog, chips, soda</td><td>$5.00</td></tr>
-         </table>
-      </div>
-    </div>
-  </div><!--/row-->
-
-  <div class="menuBottom">
-    <p class="text-center">We love to cater your office party, outing, or birthday<br>
-      <a href="tel:5052472323"><i class="fa fa-phone"></i>&nbsp;(505) 247-2323</a></p>
-      <br><p class="text-center">Prices and availability are subject to change without notice.</p>
+    </div><!--/row-->
   </div>
+
+    <div class="menuBottom">
+      <p class="text-center">We love to cater your office party, outing, or birthday<br>
+        <a href="tel:5052472323"><i class="fa fa-phone"></i>&nbsp;(505) 247-2323</a></p>
+        <br><p class="text-center">Prices and availability are subject to change without notice.</p>
+    </div>
+
+
 
     </div><!--/container-->
     <?php require_once ("lib/footer.php"); ?>

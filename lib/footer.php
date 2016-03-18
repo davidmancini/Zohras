05bc5e1773ef133f7b267557046611dc3191cd76
David Mancini <mancini.david@gmail.com>
2016-03-18 10:14:55 -0600

    Added logo to footer.

--- lib/footer.php
+++ lib/footer.php
@@ -2,6 +2,7 @@
   <div class="container-fluid"><!--Footer container-->
 		<div class="row footerRow"><!--Footer row-->
 			<div class="col-xs-12 text-center">
+        <img src="images/zohra-logo.png" alt="Zohra's Logo" class="img-responsive pull-left" width="150">
         <p>Copyright © 2015-<?php echo date("Y");?> Zohra's Restaurant</p>
         <a href="//madebymancini.com" target="_blank">Website designed and maintained by David Mancini</a><br>
         <a href="//madebymancini.com" target="_blank"><img src="images/davidmancini.jpg" alt="Made By David Mancini"></a>
